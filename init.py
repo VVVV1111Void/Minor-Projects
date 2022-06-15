0466044ac753d3c919cd2faafc356cc1bbc5dba5
Void <lawrencecastillo424@gmail.com>
2022-06-15 15:19:11 +0800

    d

--- init.py
+++ init.py
@@ -1,31 +1,30 @@
 import numpy as np
 import tkinter as tk
 from tkinter import ttk
-
-class Calculator(tk.Tk):
+class Calculator_OOP(tk.Tk):
     def __init__(self, title):
         super().__init__()
         self.title(title)
+        self.geometry('250x550')
 
+        self.mainframe = ttk.Frame(self, padding='3 3 12 12')
+        self.mainframe.pack()
+        # self.mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
 
+        self.expression = ""
+        self.shown_equation = tk.StringVar()
+        self.equation_field = tk.Entry(self, textvariable=self.shown_equation)
+        self.equation_field.pack()
+        # self.columnconfigure(0, weight=1)
+        # self.rowconfigure(0, weight=1)
+    def update(self, number):
+        return (self.expression + number)
 
-
-
-
-    def add(self, val1, val2) -> float:
-        yield val1 + val2
-
-    def substract(self, val1, val2) -> float:
-        yield val1 - val2
-    
-    def multiply(self, val1, val2) -> float:
-        yield val1 * val2
-    
-    def divide(self, val1, val2) -> float:
-        yield val1 / val2
-    
-    
+    def evaluate(self, equation):
+        return str(eval(expression))
+    def clear(self):
+        self.expression = ""
 
 if __name__ == "__main__":
-    app = Calculator(f'Calculator')
+    app = Calculator_OOP(f'Calculator')
     app.mainloop()
